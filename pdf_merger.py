53e46d0d299661a38bd2b3a2f4e3d333104dd3fe
Gunjan Vaishnav <gunjanjvaishnav@gmail.com>
2024-05-01 18:44:37 +0200

    Update pdf_merger.py
    functionalities updated: 1. can be used to merge files from multiple folders.
    2. Selected file can be removed from list of selection by 'del' key

--- pdf_merger.py
+++ pdf_merger.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from PyPDF2 import PdfWriter
+from PyPDF2 import PdfWriter, PdfReader
 import os
 
 
@@ -16,11 +16,14 @@ class PDFMergerApp:
         self.file_listbox.bind("<Button-1>", self.on_select_start)
         self.file_listbox.bind("<B1-Motion>", self.on_drag)
         self.file_listbox.bind("<ButtonRelease-1>", self.on_select_end)
+        self.file_listbox.bind("<Delete>", self.delete_selected_files)
 
         self.create_widgets()
 
         self.dragged_items = []
         self.drag_start_index = None
+
+        self.selected_files = []  # List to store full file paths
 
     def create_widgets(self):
         self.select_files_btn = tk.Button(self.master, text="Select Files", command=self.select_files)
@@ -37,13 +40,21 @@ class PDFMergerApp:
         files = filedialog.askopenfilenames(filetypes=[("PDF files", "*.pdf")])
         for file_path in files:
             self.file_listbox.insert(tk.END, os.path.basename(file_path))
+            self.selected_files.append(file_path)  # Store full file path
 
     def merge_pdfs(self):
-        files_to_merge = [self.file_listbox.get(idx) for idx in range(self.file_listbox.size())]
-        if files_to_merge:
+        if self.selected_files:
             merger = PdfWriter()
-            for file in files_to_merge:
-                merger.append(file)
+            l=[]
+            for file_path in self.selected_files:
+                l.append(file_path)
+
+            for path in l:
+                with open(path, 'rb') as file:
+                    reader = PdfReader(file)
+                    for page in reader.pages:
+                        merger.add_page(page)
+
             merged_filename = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")])
             if merged_filename:
                 merger.write(merged_filename)
@@ -75,6 +86,11 @@ class PDFMergerApp:
         self.drag_start_index = None
         self.dragged_items = []
 
+    def delete_selected_files(self, event):
+        selected_indices = self.file_listbox.curselection()
+        for index in selected_indices[::-1]:
+            del self.selected_files[index]
+            self.file_listbox.delete(index)
 
 def main():
     root = tk.Tk()
